95c1bb2114980422c51dddc630d98d80421be5d2
sondengs <sondengs@gmail.com>
2019-06-07 14:17:21 +0900

    test

---
+++ app/src/main/java/com/esansoft/oasis/ui/son/SonActivity.java
@@ -0,0 +1,15 @@
+package com.esansoft.oasis.ui.son;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.esansoft.oasis.R;
+
+public class SonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_son);
+    }
+}
